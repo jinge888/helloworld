9256187c19fa724e186726c43ead2f3c74e2c620
liaoxiaojin <liaoxiaojin2009@163.com>
2017-12-09 16:12:50 +0800

    添加注释

--- src/main/java/com/yaolala/dobigthing/dao/ResultSet2EntityMapping.java
+++ src/main/java/com/yaolala/dobigthing/dao/ResultSet2EntityMapping.java
@@ -3,7 +3,18 @@ package com.yaolala.dobigthing.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * 数据库结果集到实体类的映射接口
+ * @author liaoxiaojin
+ *
+ */
 public interface ResultSet2EntityMapping {
 
+	/**
+	 * 将数据库中的一行转换成一个实体对象
+	 * @param rs sql查询结果的一行
+	 * @return 一个实体类对象
+	 * @throws SQLException
+	 */
 	Object mapping(ResultSet rs) throws SQLException;
 }
